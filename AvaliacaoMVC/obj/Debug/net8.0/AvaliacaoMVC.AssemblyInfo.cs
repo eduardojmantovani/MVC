bceb44ee13dc39a1673b824fa8d46b820021f86a
eduardojmantovani <eduardo.acessabrasil@gmail.com>
2024-05-22 16:25:33 -0300

    ignore

--- AvaliacaoMVC/obj/Debug/net8.0/AvaliacaoMVC.AssemblyInfo.cs
+++ AvaliacaoMVC/obj/Debug/net8.0/AvaliacaoMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaliacaoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4269a3b817e1ef7571e37f6f30800dc3b995bce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c123b42e4c73ed205e6e061db25d209fdb14e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaliacaoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaliacaoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
